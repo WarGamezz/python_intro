97a32a213d1945e183ce97941e999406ccf62184
WarGamezz <widofmaker@gmail.com>
2019-07-05 08:58:40 -0400

    Styling

--- python_intro.py
+++ python_intro.py
@@ -1,5 +1,5 @@
 # This is my introduction to programming. I have no previous programming experience.
-# Created using multiple sources including: tutorialspoint.com; python-course.eu; Corey Schafer's YouTube tutorial. I do not mean to infringe any copyright laws, this file was compiled to put all of the knowledge required in one place. If there's any issue with the copyrights, please message me and I will delete requested data immediately.
+# Created using multiple sources including: tutorialspoint.com; python-course.eu; w3schools.com; Corey Schafer's YouTube tutorial. I do not mean to infringe any copyright laws, this file was compiled to put all of the knowledge required in one place. If there's any issue with the copyrights, please message me and I will delete requested data immediately.
 # Using Visual Studio Code on Windows machine, using better comments extension for better visuals
 # Word wrap is set.
 # Git https://github.com/WarGamezz/python_intro
