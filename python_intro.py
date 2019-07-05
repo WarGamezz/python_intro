b87688c6bc7395ce6684f710ad911a07a22a449d
WarGamezz <widofmaker@gmail.com>
2019-07-05 08:56:39 -0400

    Corrected styling and copyright information.

--- python_intro.py
+++ python_intro.py
@@ -2,7 +2,7 @@
 # Created using multiple sources including: tutorialspoint.com; python-course.eu; Corey Schafer's YouTube tutorial. I do not mean to infringe any copyright laws, this file was compiled to put all of the knowledge required in one place. If there's any issue with the copyrights, please message me and I will delete requested data immediately.
 # Using Visual Studio Code on Windows machine, using better comments extension for better visuals
 # Word wrap is set.
-# Git https://github.com/WarGamezz/variables
+# Git https://github.com/WarGamezz/python_intro
 
 
 # comment
